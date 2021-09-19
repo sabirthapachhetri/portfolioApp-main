775bf7e1810dfebd8780514f8db27a6f8b15d808
Sabir <sabirthapa19@gmail.com>
2021-09-19 21:41:21 +0545

    graph added

--- portfolioApp/portfolioView.swift
+++ portfolioApp/portfolioView.swift
@@ -9,18 +9,24 @@ struct portfolioView: View {
                 VStack{
                     HStack{
                         Text(model.symbol ?? "").bold()
-                            .frame(width: 68, height: 38,alignment: .leading)
+                            .frame(width: 168, height: 38,alignment: .leading)
                             .offset(x: 20, y: 0)
     //                    Text(model.securityName ?? "")
+                        
+                        if (model.percentageChange ?? 0.0 <= 0.0) {
+                            Image("miniRed")
+                        }
+                        else {
+                            Image("miniGreen")
+                        }
                         Text(String(model.lastTradedPrice ?? 0.0))
-                            .frame(width: 68, height: 38,alignment: .leading)
+                            .frame(width: 150, height: 38,alignment: .leading)
                             .offset(x: 90, y: 0)
-                        
-                        
+                            
                     }
                 }
            
-                .frame(width: 350, height: 56)
+                .frame(width: 380, height: 66)
                 .background(Color.white)
                 .cornerRadius(15)
                 .shadow(color: Color(#colorLiteral(red: 0.1411764771, green: 0.3960784376, blue: 0.5647059083, alpha: 1)).opacity(0.3), radius: 10.0)
@@ -45,6 +51,7 @@ struct portfolioView: View {
         })
             }
         }
+func changeGraph(){}
 
 
 struct portfolioView_Previews: PreviewProvider {
